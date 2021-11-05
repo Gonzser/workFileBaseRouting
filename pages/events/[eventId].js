eb9d61bb480da70c08d812626166f6fc0863f941
gonzser <gonzser@gmail.com>
2021-11-05 01:26:27 -0300

    r.ok almost done with styles

---
+++ pages/events/[eventId].js
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/EventSummary";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventContent from "../../components/event-detail/EventContent";
+
+function DetailEvent() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <div>
+        <h1>NO Event Found</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Fragment>
+        <EventSummary title={event.title} />
+        <EventLogistics
+          date={event.date}
+          address={event.location}
+          image={event.image}
+          imageAlt={event.title}
+        />
+        <EventContent>
+          <p>{event.description}</p>
+        </EventContent>
+      </Fragment>
+    </div>
+  );
+}
+
+export default DetailEvent;
---
+++ pages/events/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
+
+function AllEventsPage() {
+  const events = getAllEvents();
+
+  return (
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
