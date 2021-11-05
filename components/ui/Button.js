800797fc7067699fab88ec80566aa2b43bf36243
gonzser <gonzser@gmail.com>
2021-11-05 08:22:26 -0300

    R.ok complete project basic next

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -2,18 +2,17 @@ import Link from "next/Link";
 import classes from "./Button.module.css";
 
 function Button(props) {
-  if (props.link) {
+  if (props.link)
     return (
       <Link href={props.link}>
         <a className={classes.btn}>{props.children}</a>
       </Link>
     );
-    return (
-      <button className={classes.btn} onClick={props.onClick}>
-        {props.children}
-      </button>
-    );
-  }
+  return (
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
